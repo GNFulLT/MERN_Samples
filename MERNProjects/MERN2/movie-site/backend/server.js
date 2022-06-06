c32acb1198d568bc7195349533745b9d05b5c26f
GnfFullt <73427021+GNFulLT@users.noreply.github.com>
2022-06-06 22:16:46 +0300

    Second Project First Commit

---
+++ MERNProjects/MERN2/movie-site/backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import movies from './api/movies.route.js'
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true})); 
+
+app.use("/api/v1/movies", movies);
+
+app.use('*', (req,res)=>{
+    res.status(404).json({error: "not found"})
+});
+
+export default app;
